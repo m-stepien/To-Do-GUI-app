02460279556dc6505ee86f32dcf4d0f80d330cea
Mateusz <m.stepien1606@gmail.com>
2022-01-06 18:31:59 +0100

    Adding navbar and option to show complete task

--- to_do.py
+++ to_do.py
@@ -3,11 +3,36 @@ import tkinter as tk
 from tkinter import ttk
 
 
+class NavBar(ttk.Frame):
+    def __init__(self, master, display):
+        super().__init__()
+        self.nav_frame = ttk.Frame(master)
+        self.nav_frame.grid(row=0, column=0)
+        self.display = display
+        self.button_task_to_do = ttk.Button(
+            self.nav_frame, text="Task", command=lambda: app.open_task())
+        self.button_task_complete = ttk.Button(
+            self.nav_frame, text="Complete", command=lambda: app.open_complete())
+        self.button_task_statistic = ttk.Button(self.nav_frame, text="Statistic")
+        self.button_task_to_do.grid(row=0, column=0)
+        self.button_task_complete.grid(row=0, column=1)
+        self.button_task_statistic.grid(row=0, column=2)
+
+
+class Statistic(ttk.Frame):
+    def __init__(self, master, file):
+        super().__init__()
+        self.data_sel = ttk.Combobox(self.frame, width=27)
+        self.frame = ttk.Frame(master, borderwidth=5, padding=1)
+        self.button_next = ttk.Button(self.frame, text=">", command=lambda: self.next_fn())
+        self.button_back = ttk.Button(self.frame, text="<", command=lambda: self.back_fn())
+
+
 class Display(ttk.Frame):
     def __init__(self, master, file):
         super().__init__()
         self.file = file
-        self.frame = ttk.Frame(master, borderwidth=5, padding=20)
+        self.frame = ttk.Frame(master, borderwidth=5, padding=1)
         self.task_box = tk.Listbox(self.frame, bd=3, cursor='hand2', height=15, width=40,
                                    activestyle='none', relief='sunken', bg='grey', font=(
                                        "Ariel", 12))
@@ -16,7 +41,7 @@ class Display(ttk.Frame):
 
         self.taskbox_item_load(self.task_box, self.file)
         self.task_box.grid(column=3, row=3)
-        self.frame.grid(column=0, row=0)
+        self.frame.grid(column=1, row=1)
         self.task_box.config(yscrollcommand=self.scrollbar.set)
         self.scrollbar.config(command=self.task_box.yview)
 
@@ -32,11 +57,25 @@ class mn_win(ttk.Frame):
         super().__init__()
         self.file = file
         self.my_Frame = ttk.Frame(master)
-        self.my_Frame.pack()
         self.display = Display(self.my_Frame, self.file)
         self.option = Option(self.my_Frame, self.display)
-        self.display.pack()
-        self.option.pack()
+        self.nav = NavBar(self.my_Frame, self.display)
+        self.nav.grid(column=0, row=0)
+        self.display.grid(column=0, row=1)
+        self.option.grid(column=1, row=2)
+        self.my_Frame.grid(column=0, row=0)
+
+    def open_task(self):
+        self.display = Display(self.my_Frame, "task.txt")
+
+    def open_complete(self):
+        # nie podoba mi się
+        self.option.save_change()
+        self.display = Display(self.my_Frame, "complete.txt")
+
+    def open_statistic(self):
+        self.option.save_change()
+        self.display = Statistic()
 
 
 class sm_win():
@@ -66,19 +105,26 @@ class sm_win():
             li_box.insert(ind, task)
             self.win.destroy()
 
+
 class Option(ttk.Frame):
     def __init__(self, master, dis, but_pad=5):
         super().__init__()
         self.master = master
-        self.dis=dis
+        self.dis = dis
         self.option_frame = ttk.Frame(master, padding=30)
-        self.option_frame.grid(column=2, row=0)
-        self.button_add = ttk.Button(self.option_frame, text="ADD", padding=but_pad, command=lambda: self.addTask())
-        self.button_del = ttk.Button(self.option_frame, text="DELETE", padding=but_pad, command=lambda: self.dis.task_box.delete("anchor"))
-        self.button_edit = ttk.Button(self.option_frame, text="EDIT", padding=but_pad, command=lambda: self.eddTask())
-        self.button_clear_all = ttk.Button(self.option_frame, text="CLEAR ALL", padding=but_pad, command=lambda: self.dis.task_box.delete(0, "end"))
-        self.button_save = ttk.Button(self.option_frame, text="SAVE", padding=but_pad, command=lambda: self.save_change())
-        self.button_done = ttk.Button(self.option_frame, text="DONE", padding=but_pad, command=lambda: self.done_task())
+        self.option_frame.grid(column=2)
+        self.button_add = ttk.Button(self.option_frame, text="ADD",
+                                     padding=but_pad, command=lambda: self.addTask())
+        self.button_del = ttk.Button(self.option_frame, text="DELETE",
+                                     padding=but_pad, command=lambda: self.dis.task_box.delete("anchor"))
+        self.button_edit = ttk.Button(self.option_frame, text="EDIT",
+                                      padding=but_pad, command=lambda: self.eddTask())
+        self.button_clear_all = ttk.Button(
+            self.option_frame, text="CLEAR ALL", padding=but_pad, command=lambda: self.dis.task_box.delete(0, "end"))
+        self.button_save = ttk.Button(self.option_frame, text="SAVE",
+                                      padding=but_pad, command=lambda: self.save_change())
+        self.button_done = ttk.Button(self.option_frame, text="DONE",
+                                      padding=but_pad, command=lambda: self.done_task())
         self.button_edit.grid(column=2, row=0, pady=5)
         self.button_del.grid(column=2, row=1, pady=5)
         self.button_add.grid(column=2, row=2, pady=5)
@@ -87,14 +133,12 @@ class Option(ttk.Frame):
         self.button_done.grid(column=2, row=5, pady=5)
         self.option_frame.grid(sticky='nesw')
 
-
     def addTask(self):
         sm_win(self.dis.task_box, 0, entry_text="", i='end', title="Add Task")
 
-
     def eddTask(self):
-        sm_win(self.dis.task_box, 1, entry_text=self.dis.task_box.get("anchor"), i=self.dis.task_box.curselection()[0], title="Edit Task")
-
+        sm_win(self.dis.task_box, 1, entry_text=self.dis.task_box.get("anchor"),
+               i=self.dis.task_box.curselection()[0], title="Edit Task")
 
     def save_change(self):
         ln = self.dis.task_box.size()
@@ -111,10 +155,10 @@ class Option(ttk.Frame):
         self.dis.task_box.delete("anchor")
 
 
-
 root = tk.Tk()
 ttk.Style().configure("TButton", relief='sunken', background='black')
 root.title("To do")
 root.geometry("600x350")
-mn_win(root, "task.txt")
+app = mn_win(root, "task.txt")
+app.grid()
 root.mainloop()
